001e1a9f6843809e920bde12230bf54981a462ce
thenaughtychid <f2014768@pilani.bits-pilani.ac.in>
2017-07-22 02:35:04 +0530

    http-service

--- ionic/src/providers/http-service.ts
+++ ionic/src/providers/http-service.ts
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
-import { Http,Headers, RequestOptions } from '@angular/http';
+import { Http,Headers, RequestOptions, Response  } from '@angular/http';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/map';
@@ -18,6 +18,84 @@ export class HttpService {
 		console.log('Hello HttpUtils Provider');
 	}
 
+	login_as_student(BITS_mail: string, Password: string) {
+    	let body_obj: any = { BITS_mail, Password };
+        const body = JSON.stringify(body_obj);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('nodejs-rest-api.herokuapp.com/api/login_as_student', body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+	}
+	
+	login_as_admin(admin_mail: string, Password: string) {
+    	let body_obj: any = { admin_mail, Password };
+        const body = JSON.stringify(body_obj);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('nodejs-rest-api.herokuapp.com/api/login_as_admin', body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+	}
+
+	list_of_hostels() {
+        return this.http.get('nodejs-rest-api.herokuapp.com/api/list_of_hostels')
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+	}
+
+	list_of_hostel_students(hostel_name: string) {
+        let body_obj: any = { hostel_name };
+        const body = JSON.stringify(body_obj);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('nodejs-rest-api.herokuapp.com/api/list_of_hostel_students', body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+	}
+
+	student_details(bits_id: string) {
+        let body_obj: any = { bits_id };
+        const body = JSON.stringify(body_obj);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('nodejs-rest-api.herokuapp.com/api/student_details', body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+	}
+
+	last_wash_status(bits_id: string) {
+        let body_obj: any = { bits_id };
+        const body = JSON.stringify(body_obj);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('nodejs-rest-api.herokuapp.com/api/last_wash_status', body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+	}
+
+	list_of_urgency_requests() {
+        return this.http.get('nodejs-rest-api.herokuapp.com/api/list_of_urgency_requests')
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+	}
+
+	student_profile(bits_id: string) {
+        let body_obj: any = { bits_id };
+        const body = JSON.stringify(body_obj);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('nodejs-rest-api.herokuapp.com/api/student_profile', body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+	}
+
+	download_excel() {
+        return this.http.get('nodejs-rest-api.herokuapp.com/api/download_excel')
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+	}
+
+	analysis() {
+        return this.http.get('nodejs-rest-api.herokuapp.com/api/analysis')
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+	}
+		
 
 	getData(url){
 		return this.http.get(url)
