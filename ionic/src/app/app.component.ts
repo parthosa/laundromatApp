7270606bd6ca3f8019181aa686835f4681572c3f
thenaughtychid <f2014768@pilani.bits-pilani.ac.in>
2017-07-29 01:33:54 +0530

    Push Notifications

--- ionic/src/app/app.component.ts
+++ ionic/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component,ViewChild } from '@angular/core';
-import { Platform,MenuController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { NavController } from 'ionic-angular';
@@ -12,19 +12,31 @@ import { HomePage } from '../pages/home/home';
 export class LaundromatApp {
   @ViewChild('mainNav') navCtrl: NavController
 
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,public menuCtrl: MenuController,) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public menuCtrl: MenuController, ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
       this.menuCtrl.enable(false);
+
+      // Enable to debug issues.
+      // window["plugins"].OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+
+      var notificationOpenedCallback = function (jsonData) {
+        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+      };
+
+      window["plugins"].OneSignal
+        .startInit("6d075c03-ae67-48ea-9009-601a111fda94", "942120250462")
+        .handleNotificationOpened(notificationOpenedCallback)
+        .endInit();
     });
   }
 
-  logout(){
+  logout() {
     console.log(this.navCtrl);
     this.navCtrl.setRoot(HomePage);
   }
--- ionic/src/pages/home/home.ts
+++ ionic/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController,ToastController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { StudentPage } from '../student/student';
 import { AdminLoginPage } from '../admin-login/admin-login';
 import { User } from '@ionic/cloud-angular';
@@ -7,70 +7,78 @@ import { Storage } from '@ionic/storage';
 import { HttpService } from '../../providers/http-service';
 // import { User } from '@ionic/cloud-angular';
 import { GooglePlus } from 'ionic-native';
+import {
+  Push,
+  PushToken
+} from '@ionic/cloud-angular';
 
 @Component({
-	selector: 'page-home',
-	templateUrl: 'home.html'
+  selector: 'page-home',
+  templateUrl: 'home.html'
 })
 export class HomePage {
 
 
-	constructor(public navCtrl: NavController, public user: User,public toastCtrl: ToastController,public storage: Storage,private httpService: HttpService) {
+  constructor(public navCtrl: NavController, public user: User, public toastCtrl: ToastController, public storage: Storage, public push: Push, private httpService: HttpService) {
+    this.navCtrl.setRoot(StudentPage);
+    this.push.register().then((t: PushToken) => {
+      return this.push.saveToken(t);
+    }).then((t: PushToken) => {
+      this.storage.set('token', t.token);
+      console.log('Token saved:', t.token);
+    });
+  }
 
-	}
+  goToAdminPage() {
+    // this.httpService.postData('https://jsonplaceholder.typicode.com/posts/',{user:'psarthi16@gmail.com'}).then(post=>
+    // 	console.log(post),
+    // 	error=>console.log(error));
+    this.navCtrl.push(AdminLoginPage);
+  }
 
-	goToAdminPage(){
-		// this.httpService.postData('https://jsonplaceholder.typicode.com/posts/',{user:'psarthi16@gmail.com'}).then(post=>
-		// 	console.log(post),
-		// 	error=>console.log(error));
-		this.navCtrl.push(AdminLoginPage);
-	}
-
-	studentLogin(){
-		console.log(1);
-		// GooglePlus.login({
-  //         'webClientId': '931784175657-tnlaleval048phhgbrgbmeqi2hh64pmq.apps.googleusercontent.com'
-  //       }).then((res) => {
-  //       	this.user.details = res;
-  //       	this.httpService.postData('/api/get_id/',this.user.details).then(
-  //       		(response)=>{
-  //       			if(response.status == 1){
-   //        			this.user.details['id'] = response.id;
-   //             this.storage.set('session_key', response.session_key);
-	 //        			this.navCtrl.setRoot(StudentPage);
-  //       			}
-  //       			else{
-  //       				this.toastCtrl.create({
-		// 		              message: response.message,
-		// 		              duration: 3000,
-		// 		            }).present();
-  //       			}
-  //       		},
-  //       		(err)=>{
-  //       			this.toastCtrl.create({
-		// 		              message: 'Try Again',
-		// 		              duration: 3000,
-		// 		            }).present();
-  //       		});
-  //       }, (err) => {
-		// 	this.toastCtrl.create({
-		// 		              message: 'Try Again',
-		// 		              duration: 3000,
-		// 		            }).present();
-		// 	// this.navCtrl.setRoot(StudentPage);
-		// });
-		this.navCtrl.setRoot(StudentPage);
-
-	}
+  studentLogin() {
+    console.log(1);
+    // GooglePlus.login({
+    //         'webClientId': '931784175657-tnlaleval048phhgbrgbmeqi2hh64pmq.apps.googleusercontent.com'
+    //       }).then((res) => {
+    //       	this.user.details = res;
+    //       	this.httpService.postData('/api/get_id/',this.user.details).then(
+    //       		(response)=>{
+    //       			if(response.status == 1){
+    //        			this.user.details['id'] = response.id;
+    //             this.storage.set('session_key', response.session_key);
+    //        			this.navCtrl.setRoot(StudentPage);
+    //       			}
+    //       			else{
+    //       				this.toastCtrl.create({
+    // 		              message: response.message,
+    // 		              duration: 3000,
+    // 		            }).present();
+    //       			}
+    //       		},
+    //       		(err)=>{
+    //       			this.toastCtrl.create({
+    // 		              message: 'Try Again',
+    // 		              duration: 3000,
+    // 		            }).present();
+    //       		});
+    //       }, (err) => {
+    // 	this.toastCtrl.create({
+    // 		              message: 'Try Again',
+    // 		              duration: 3000,
+    // 		            }).present();
+    // 	// this.navCtrl.setRoot(StudentPage);
+    // });
+  }
 
 
-	// showAlert(message,title=''){
-	// 	let alert = this.alertCtrl.create({
-	//       title: title,
-	//       subTitle: message,
-	//       buttons: ['OK']
-	//     });
-	//     alert.present();
-	// }
+  // showAlert(message,title=''){
+  // 	let alert = this.alertCtrl.create({
+  //       title: title,
+  //       subTitle: message,
+  //       buttons: ['OK']
+  //     });
+  //     alert.present();
+  // }
 }
 
